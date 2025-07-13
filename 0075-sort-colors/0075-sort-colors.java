350714e8db328e2cb9fde7b7a3ec92065143006c
Lishika <Lishika9643@gmail.com>
2025-07-14 03:17:57 +0530

    Time: 0 ms (100%), Space: 42 MB (49.54%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count[]=new int[3];
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]++;
+        }int j=0;
+        for(int i=0;i<count.length;i++){ 
+            int cnt=count[i];
+            while(cnt>0){
+                nums[j]=i;
+                j++;
+                cnt--;
+            }  
+        }
+    }
+}
