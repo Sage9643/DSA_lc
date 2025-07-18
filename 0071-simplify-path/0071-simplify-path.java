e2bd4aaa1b716c3ecd4f27ff74e73551c407a410
Lishika <Lishika9643@gmail.com>
2025-07-18 23:58:46 +0530

    Time: 7 ms (21.87%), Space: 44.2 MB (21.1%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -2,18 +2,20 @@ class Solution {
     public String simplifyPath(String path) {
         String directories[]=path.split("/+");//removes slashes from path and will store all directory names
         Deque<String> deque=new LinkedList<>();//obtaing the abspath
+
         for(int i=0;i<directories.length;i++){
             if(directories[i].equals("..") && !deque.isEmpty()){//removes previous directory if stack not empty
                 deque.removeLast();
             }else if(directories[i].equals(".") || directories[i].equals("..")){
-                continue;
+                continue;//if deque empty but still .. occurs then ignore them
             }
             else{
                 deque.addLast(directories[i]);
             }
-        }StringBuilder absPath=new StringBuilder();
+        }
+        StringBuilder absPath=new StringBuilder();
         while(!deque.isEmpty()){
-            if(!deque.getFirst().equals(".") && !deque.getFirst().equals("")){
+            if(!deque.getFirst().equals("")){
                 absPath.append("/");
                 absPath.append(deque.removeFirst());
             }else{deque.removeFirst();}
