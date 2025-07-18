cd8e75c44db2804c88d6bd66e5a82f2f51744d06
Lishika <Lishika9643@gmail.com>
2025-07-18 23:57:31 +0530

    Time: 7 ms (21.87%), Space: 43.7 MB (48.99%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String directories[]=path.split("/+");//removes slashes from path and will store all directory names
+        Deque<String> deque=new LinkedList<>();//obtaing the abspath
+        for(int i=0;i<directories.length;i++){
+            if(directories[i].equals("..") && !deque.isEmpty()){//removes previous directory if stack not empty
+                deque.removeLast();
+            }else if(directories[i].equals(".") || directories[i].equals("..")){
+                continue;
+            }
+            else{
+                deque.addLast(directories[i]);
+            }
+        }StringBuilder absPath=new StringBuilder();
+        while(!deque.isEmpty()){
+            if(!deque.getFirst().equals(".") && !deque.getFirst().equals("")){
+                absPath.append("/");
+                absPath.append(deque.removeFirst());
+            }else{deque.removeFirst();}
+            
+        }return absPath.length()==0 ? "/" : absPath.toString();
+
+
+    }
+}
