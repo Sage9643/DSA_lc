4af65182da547024413a7fad38726aa350886088
Lishika <Lishika9643@gmail.com>
2025-07-28 15:36:02 +0530

    Time: 8 ms (80.38%), Space: 40.9 MB (84.22%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int countMaxOrSubsets(int[] nums) {
-        int max=0;
+    //public int countMaxOrSubsets(int[] nums) {
+       /* int max=0;
         for(int i=0;i<nums.length;i++){
             max|=nums[i];
         }
@@ -19,5 +19,35 @@ class Solution {
                 count++;
             }
         }return count;
+        */
+
+    
+    int max = 0, count = 0;
+
+    public int countMaxOrSubsets(int[] nums) {
+        // Step 1: Find the maximum OR that can be achieved
+        for (int num : nums) {
+            max |= num;  // bitwise OR assignment
+        }
+
+        // Step 2: Start DFS
+        dfs(nums, 0, 0);
+        return count;
+    }
+
+    // DFS function
+    void dfs(int[] nums, int index, int curr) {
+        if (index == nums.length) {
+            if (curr == max) count++;
+            return;
+        }
+
+        // Include current element in subset
+        dfs(nums, index + 1, curr | nums[index]);
+
+        // Exclude current element from subset
+        dfs(nums, index + 1, curr);
     }
 }
+
+    
