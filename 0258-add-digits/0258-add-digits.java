c86ff32c518687b9744547687f8e52dd439344df
Lishika <Lishika9643@gmail.com>
2025-08-04 02:26:01 +0530

    Time: 1 ms (42.99%), Space: 40.8 MB (71.59%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,13 +1,15 @@
 class Solution {
     public int addDigits(int num) {
-        if(num==0)return 0;
-        while(num /10!=0){
-            int dig=num;
-            num=0;
-            while(dig!=0){
-                num+=dig%10;
-                dig=dig/10;
-            }
-        }return num;
+        // if(num==0)return 0;
+        // while(num /10!=0){
+        //     int dig=num;
+        //     num=0;
+        //     while(dig!=0){
+        //         num+=dig%10;
+        //         dig=dig/10;
+        //     }
+        // }return num;
+        if (num==0) return 0;
+        return 1+(num-1)%9;
     }
 }
