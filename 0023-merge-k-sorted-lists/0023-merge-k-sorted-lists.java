e1bbf3cccbcc95872c08962226e0aa43967812d4
Lishika <Lishika9643@gmail.com>
2025-08-09 15:09:25 +0530

    Time: 4 ms (78.74%), Space: 44.5 MB (64.94%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,34 +10,20 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0) return null;
-        ArrayList<ListNode> arr=new ArrayList<>();
-        for(int i=0;i<lists.length;i++){
-            arr.add(lists[i]);
-        }while(arr.size()>1){
-            mergeSort(arr.get(0),arr.get(1),arr);
+        PriorityQueue<ListNode> heap=new PriorityQueue<>((head1,head2)-> head1.val-head2.val);
+        for(ListNode l:lists){
+            if(l!=null) heap.add(l);
         }
-        return arr.get(0);
+        ListNode head=null,tail=null;
+        while(!heap.isEmpty()){
+            ListNode node=heap.remove();
 
-
-
-    }private void mergeSort(ListNode head1,ListNode head2,ArrayList<ListNode> arr){
-        ListNode tempHead=new ListNode(-1);
-        ListNode temp=tempHead;
-
-        while(head1!=null && head2!=null){
-            if(head1.val<=head2.val){
-                temp.next=head1;
-                head1=head1.next;
-                
-            }else{
-                temp.next=head2;
-                
-                head2=head2.next;
-            }temp=temp.next;
-        }if(head1!=null) temp.next=head1;
-        if(head2!=null) temp.next=head2;
-        arr.remove(1);arr.remove(0);arr.add(tempHead.next);
-
+            if(head==null) head=tail=node;
+            else{
+                tail.next=node;
+                tail=tail.next;
+            }
+            if(node.next!=null) heap.add(node.next);
+        }return head;
     }
 }
