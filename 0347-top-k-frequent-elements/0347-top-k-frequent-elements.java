b95018a8eed0e202feff58b668b866b2f070ac12
Lishika <Lishika9643@gmail.com>
2025-08-11 02:08:49 +0530

    Time: 11 ms (91.79%), Space: 49.2 MB (13.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(Integer num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        PriorityQueue<Pair> heap=new PriorityQueue<>();
+        for(int num:nums) map.put(num,map.getOrDefault(num,0)+1);
+
+        List<Integer> bucket[]=new List[nums.length+1];
+
         for(Integer key:map.keySet()){
-            heap.add(new Pair(key,map.get(key)));
+            Integer freq=map.get(key);
+            if(bucket[freq]==null) bucket[freq]=new ArrayList<>();
+            bucket[freq].add(key);
         }
-        int arr[]=new int[k];
-        for(int i=0;i<k;i++){
-            arr[i]=heap.remove().num;
-        }return arr;
-    }
-}class Pair implements Comparable<Pair>{
-    int num;
-    int freq;
-    public Pair(int num,int freq){
-        this.num=num;
-        this.freq=freq;
-    }public int compareTo(Pair pair1){
-        return pair1.freq-this.freq;
+        int index=0;
+        int ans[]=new int[k];
+        for(int freq=bucket.length-1;freq>=0 && index<k;freq--){
+            if(bucket[freq]!=null){
+                for(int num:bucket[freq]){
+                    ans[index++]=num;
+                    if(index==k)return ans;
+                }
+            }
+        }return ans;
     }
 }
