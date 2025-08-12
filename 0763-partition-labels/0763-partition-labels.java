d002b0889474e06f54df60e94a5448a747bee75b
Lishika <Lishika9643@gmail.com>
2025-08-13 03:41:18 +0530

    Time: 5 ms (79.32%), Space: 41.8 MB (98.33%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,28 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer> freqMap=new HashMap<>();
-        List<Integer> size=new ArrayList<>();
-
-        int freq[]=new int[26];
-        for(char ch:s.toCharArray())freq[ch-'a']++;
+        int lastIdx[]=new int[26];
+        List<Integer> partitions=new ArrayList<>();
         for(int i=0;i<s.length();i++){
-            freqMap.put(s.charAt(i),freq[s.charAt(i)-'a']-1);
-            int length=1;   
-            while(i<s.length()-1 && !freqMap.isEmpty()){
-                if(freqMap.containsKey(s.charAt(i)) && freqMap.get(s.charAt(i))==0) {
-                    freqMap.remove(s.charAt(i));
-                    size.add(length);
-                    length=0;
-                }
-                i++;
-                if(!freqMap.containsKey(s.charAt(i))){
-                    freqMap.put(s.charAt(i),freq[s.charAt(i)-'a']-1);
-                }else{
-                    freqMap.put(s.charAt(i),freqMap.get(s.charAt(i))-1);
-                
-                }length++;
-                if(freqMap.get(s.charAt(i))==0) freqMap.remove(s.charAt(i));
-            }size.add(length);
-        }return size;
+            lastIdx[s.charAt(i)-'a']=i;
+        }
+        int maxLast=0;int start=0;
+        for(int i=0;i<s.length();i++){
+            maxLast=Math.max(maxLast,lastIdx[s.charAt(i)-'a']);
+            if(i==maxLast){
+                partitions.add(maxLast-start+1);
+                start=i+1;
+            }
+        }return partitions;
     }
 }
