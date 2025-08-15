75b5e8dabeca92d0a7a292591cf0658efac219f4
Lishika <Lishika9643@gmail.com>
2025-08-15 16:57:50 +0530

    Time: 3 ms (99.39%), Space: 57.5 MB (99.39%) - LeetHub

--- 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
+++ 2661-first-completely-painted-row-or-column/2661-first-completely-painted-row-or-column.java
@@ -1,24 +1,26 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int m=mat.length;
-        int n=mat[0].length;
-        HashMap<Integer,int[]> map=new HashMap<>();
+        //Approach if all the elements are in range [1,m*n] and are unique
+        int m=mat.length,n=mat[0].length;
+        int[] position=new int[m*n+1];//storing index of mat elements in 1d array form
+        int remainingRows[]=new int[m];
+        int remainingCols[]=new int[n];
+
         for(int row=0;row<m;row++){
             for(int col=0;col<n;col++){
-                map.put(mat[row][col],new int[]{row,col});
+                int pos=row*n+col;
+                position[mat[row][col]]=pos;
+                remainingRows[row]++;
+                remainingCols[col]++;
             }
         }
-        int rows[]=new int[m];
-        int cols[]=new int[n];
-        Arrays.fill(rows,n);Arrays.fill(cols,m);//here each row contains n cols thatswhy filled with n and same for cols
-
         for(int i=0;i<arr.length;i++){
-            int cell[]=map.get(arr[i]);
-            rows[cell[0]]--;
-            if(rows[cell[0]]==0) return i;
+            int pos=position[arr[i]];
+            remainingRows[pos/n]--;
+            if(remainingRows[pos/n]==0) return i;
 
-            cols[cell[1]]--;
-            if(cols[cell[1]]==0) return i;
+            remainingCols[pos%n]--;
+            if(remainingCols[pos%n]==0) return i;
         }
         return -1;
     }
