c960f9ef474c1be761eb526a7c5bec2c7c7e43f8
Lishika <Lishika9643@gmail.com>
2025-08-30 02:38:30 +0530

    Time: 3 ms (91.07%), Space: 43.2 MB (63.64%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.java
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -1,31 +1,20 @@
 class Solution {
     public List<String> commonChars(String[] words) {
-        HashMap<String,Integer> map=new HashMap<>();
+        int[] minfreq=new int[26];
         List<String> ans=new ArrayList<>();
+        Arrays.fill(minfreq,Integer.MAX_VALUE);
 
         String initial=words[0];
-        int freqs[]=new int[26];
-        for(char ch:initial.toCharArray()){
-            freqs[ch-'a']++;
-        }for(int i=0;i<26;i++){
-            if(freqs[i]!=0) map.put(Character.toString('a'+i),freqs[i]);
-        }
+        
         for(String word:words){
             int freq[]=new int[26];
             for(char ch:word.toCharArray()) freq[ch-'a']++;
             
             for(int i=0;i<26;i++){
-                String ltr=Character.toString('a'+i);
-                if(freq[i]==0){
-                    if(map.containsKey(ltr)) map.remove(ltr);
-                }else{
-                    if(map.containsKey(ltr)){
-                        if(map.get(ltr)>freq[i]) map.put(ltr,freq[i]);
-                    }
-                }       
+                minfreq[i]=Math.min(minfreq[i],freq[i]);
             }
-        }for(String letter:map.keySet()){
-            for(int i=0;i<map.get(letter);i++) ans.add(letter);
+        }for(int i=0;i<26;i++){
+            for(int j=0;j<minfreq[i];j++) ans.add(Character.toString('a'+i));
         }return ans;
     }
 }
