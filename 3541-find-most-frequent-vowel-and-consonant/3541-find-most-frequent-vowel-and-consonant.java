459edfe2a2f656e7cb5b1e1b55e4bae2c5572946
Lishika <Lishika9643@gmail.com>
2025-09-13 20:56:07 +0530

    Time: 1 ms (100%), Space: 42.5 MB (76.14%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -1,21 +1,17 @@
 class Solution {
     public int maxFreqSum(String s) {
-        HashMap<Character,Integer> mapCon=new HashMap<>();
-        HashMap<Character,Integer> mapVow=new HashMap<>();
-
-        int consonant=0;int vowel=0;
-
+        int arr[]=new int[26];
         for(char ch:s.toCharArray()){
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                mapVow.put(ch,mapVow.getOrDefault(ch,0)+1);
-            }else mapCon.put(ch,mapCon.getOrDefault(ch,0)+1);
+            arr[ch-'a']++;
         }
-        for(Character consonants:mapCon.keySet()){
-            if(mapCon.get(consonants)>consonant) consonant=mapCon.get(consonants);
-        }
-        for(Character vowels:mapVow.keySet()){
-            if(mapVow.get(vowels)>vowel) vowel=mapVow.get(vowels);
-        }
-        return vowel+consonant;
+        int con=0;int vow=0;
+
+        for(int i=0;i<arr.length;i++){
+            if(i==0||i==4||i==8||i==14||i==20){
+                if(arr[i]>vow) vow=arr[i];
+            }else{
+                if(arr[i]>con) con=arr[i];
+            }
+        }return con+vow;
     }
 }
