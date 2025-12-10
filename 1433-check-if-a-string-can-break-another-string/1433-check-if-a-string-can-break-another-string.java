5927ccd4c6c5efe07d6b18b4d59e26462b442c0f
Lishika <Lishika9643@gmail.com>
2025-12-10 05:51:59 +0530

    Time: 5 ms (97.4%), Space: 47.2 MB (62.5%) - LeetHub

--- 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.java
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.java
@@ -1,22 +1,39 @@
 class Solution {
     public boolean checkIfCanBreak(String s1, String s2) {
-        char[] arr1=s1.toCharArray();
-        char[] arr2=s2.toCharArray();
-        Arrays.sort(arr1);Arrays.sort(arr2);
+        
+        int arr1[]=new int[26];
+        int arr2[]=new int[26];
 
-        boolean v1=true;boolean v2=true;
-        //if s1 stronger than s2
-        for(int i=0;i<s1.length();i++){
-            if(arr1[i]<arr2[i]){
+        for(char ch:s1.toCharArray())arr1[ch-'a']++;
+        for(char ch:s2.toCharArray())arr2[ch-'a']++;
+
+        int i=0,j=0;
+        boolean v1=true,v2=true;
+        //s1 stronger than s2
+        int arr3[]=arr1.clone();
+        int arr4[]=arr2.clone();
+        while(i<26 && j<26){
+            while(i<26 && arr1[i]==0)i++;
+            while(j<26 && arr2[j]==0)j++;
+            if(i<26 && j<26 && i<j ) {
                 v1=false;break;
+            }else if(i<26 && j<26 && i>=j ){
+                if(arr2[j]>=arr1[i]){arr2[j]-=arr1[i]; arr1[i]=0;}
+                else {arr1[i]-=arr2[j];arr2[j]=0;}
             }
-        }
-        //if s2 stronger than s1
-        for(int i=0;i<s1.length();i++){
-            if(arr1[i]>arr2[i]){
+        }i=0;j=0;
+        //s2 stronger than s1
+        while(i<26 && j<26){
+            while(i<26 && arr3[i]==0)i++;
+            while(j<26 && arr4[j]==0)j++;
+            if(i<26 && j<26 && i>j) {
                 v2=false;break;
+            }else if(i<26 && j<26 && i<=j){
+                if(arr4[j]>=arr3[i]){arr4[j]-=arr3[i]; arr3[i]=0;}
+                else {arr3[i]-=arr4[j];arr4[j]=0;}
             }
-        }return v1||v2;
 
+        }
+        return v1||v2;
     }
 }
