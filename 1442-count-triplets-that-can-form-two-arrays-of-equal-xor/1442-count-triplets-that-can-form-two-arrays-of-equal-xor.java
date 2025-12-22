2b117ac401b0b22eb133c9b835d928f285003a31
Lishika <Lishika9643@gmail.com>
2025-12-23 02:51:40 +0530

    Time: 5 ms (30.65%), Space: 43.5 MB (5.26%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,18 +1,21 @@
 class Solution {
     public int countTriplets(int[] arr) {
         int cnt=0;
+        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
+        map.put(0,new ArrayList<>());map.get(0).add(-1);
+        int xor=0;
 
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=i;j<arr.length;j++){
-                int lxor=0;
-                for(int k=i;k<=j;k++){
-                    lxor^=arr[k];
-                }int rxor=0;
-                for(int k=j+1;k<arr.length;k++){
-                    rxor^=arr[k];
-                    if(lxor==rxor)cnt++;
+        for(int i=0;i<arr.length;i++){
+            xor^=arr[i];
+            if(map.containsKey(xor)){
+                for(int j=0;j<map.get(xor).size();j++){
+                    cnt+=(i-map.get(xor).get(j)-1);
                 }
             }
+
+            else if(!map.containsKey(xor)) map.put(xor,new ArrayList<>());
+            map.get(xor).add(i);
+            
         }return cnt;
     }
 }
