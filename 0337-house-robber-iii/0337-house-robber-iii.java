998dcd9ee6a7546bf0d7e411ee6272b8747cc461
Lishika <Lishika9643@gmail.com>
2026-07-08 16:29:42 +0530

    Time: 0 ms (100%), Space: 46.7 MB (44.84%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int res[]=dfs(root);
+        return Math.max(res[0],res[1]);
+    }
+    public static int[] dfs(TreeNode root){
+        if(root==null) return new int[]{0,0};
+
+        int l[]=dfs(root.left);
+        int r[]=dfs(root.right);
+
+        int inc=root.val+l[1]+r[1];
+        int exc=Math.max(l[0],l[1])+Math.max(r[0],r[1]);
+
+        return new int[]{inc,exc};
+    }
+}
