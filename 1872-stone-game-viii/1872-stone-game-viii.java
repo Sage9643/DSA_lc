e552505de07c6cc837a2a99a0ca4e226e3e1e6e1
Lishika <Lishika9643@gmail.com>
2026-08-24 22:36:28 +0530

    Time: 4 ms (66.81%), Space: 86.9 MB (56.17%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        
+        int n=stones.length;
+        int dp[]=new int[n];
+
+        int pref[]=new int[n];
+        pref[0]=stones[0];
+
+        for(int i=1;i<n;i++) pref[i]=pref[i-1]+stones[i];
+        dp[n-1]=pref[n-1];
+
+        for(int i=n-2;i>0;i--){
+            int take=pref[i]-dp[i+1];
+            int skip=dp[i+1];
+
+            dp[i]=Math.max(take,skip);
+        }
+        return dp[1];
+    }
+}
